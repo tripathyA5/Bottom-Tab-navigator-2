8eeae336739601445ea307297b277eebd07bf9c5
tripathyA5 <69359952+tripathyA5@users.noreply.github.com>
2021-01-07 21:08:02 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+
+import FbScreen from './screens/fb';
+import InScreen from './screens/in';
+
+export default class App extends React.Component {
+  render(){
+    return (
+      
+        <AppContainer />
+      
+    );
+  }
+}
+
+const TabNavigator = createBottomTabNavigator({
+  FbScreen: {screen: FbScreen},
+InScreen: {screen: InScreen},
+},
+{
+  defaultNavigationOptions: ({navigation})=>({
+    tabBarIcon: ()=>{
+      const routeName = navigation.state.routeName;
+      console.log(routeName)
+      if(routeName === "facebook"){
+        return(
+          <Text>Social Media</Text>
+        )
+      }
+    }
+  }
---
+++ screens/in.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
+export default class SearchScreen extends React.Component{
+    render(){
+        return(
+            <View>
+                <Text>
+                    Instagram
+                </Text>
+            </View>
+        )
+    }
+}
